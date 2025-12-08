7e268058a1da206688b9c563390b6624d9029e7f
KamenskiyIlya <kamenskj.ilya@gmail.com>
2025-12-08 23:12:07 +0300

    Изменил реализацию функции passcard_content

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ from datacenter.models import Passcard  # noqa: E402
 def passcard_content(passcards):
     index = int(input('Введите индекс пропуска, по которому необходима информация: \n'))
     passcard_note = passcards[index]
-    print(f'owner_name: {passcard_note.owner_name}')
-    print(f'passcode: {passcard_note.passcode}')
-    print(f'created at: {passcard_note.created_at}')
-    print(f'is_active: {passcard_note.is_active}')
-    return
+
+    passcard_details = {
+        'Имя владельца' : passcard_note.owner_name,
+        'Код пропуска' : passcard_note.passcode,
+        'Создан' : passcard_note.created_at,
+        'Активен' : passcard_note.is_active
+    }
+
+    return passcard_details
 
 if __name__ == '__main__':
     passcards = Passcard.objects.all()
@@ -22,4 +26,7 @@ if __name__ == '__main__':
 
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     print('Активных пропусков: ', len(active_passcards))
-    passcard_content(passcards)
+    
+    passcard_details = passcard_content(passcards)
+    for details, value in passcard_details.items():
+        print(f'{details}: {value}')
