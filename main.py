6404f2dd518e723faadeb812141f85ebdf58c704
KamenskiyIlya <kamenskj.ilya@gmail.com>
2025-12-08 22:35:03 +0300

    Написал основной скрипт

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
+def passcard_content(passcards):
+    index = int(input('Введите индекс пропуска, по которому необходима информация: \n'))
+    passcard_note = passcards[index]
+    print(f'owner_name: {passcard_note.owner_name}')
+    print(f'passcode: {passcard_note.passcode}')
+    print(f'created at: {passcard_note.created_at}')
+    print(f'is_active: {passcard_note.is_active}')
+    return
+
 if __name__ == '__main__':
-    # Программируем здесь
+    passcards = Passcard.objects.all()
+    active_passcards = Passcard.objects.filter(is_active=True)
+
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Активных пропусков: ', len(active_passcards))
+    passcard_content(passcards)
