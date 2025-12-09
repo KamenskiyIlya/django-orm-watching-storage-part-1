b80bd09dbed4380dc201447f55856a5ec07f8ebc
KamenskiyIlya <kamenskj.ilya@gmail.com>
2025-12-09 21:16:24 +0300

    Оптимизировал настройки с помощью пакета dj_database_url

--- settings.py
+++ settings.py
@@ -1,22 +1,21 @@
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv('ENGINE'),
-        'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD': os.getenv('PASSWORD'),
-    }
+    'default': dj_database_url.config(
+        default='DATABASE_URL',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('DB_SECRET_KEY')
 
 TIME_ZONE = 'Europe/Moscow'
 
